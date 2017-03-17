41d6aefd3cc872a52489892bdcee3755f0661e60
Alex <versmee.alexandre@gmail.com>
2017-03-17 19:41:17 +0100

    Debut de page devis en ligne

--- devis_en_ligne.php
+++ devis_en_ligne.php
@@ -3,69 +3,89 @@
 <head>
 	<?php include 'includes/metas.php' ?>
 
-	<title>Assurance Versmée & Hautcoeur Valenciennes</title>
+    <title>Assurance Versmée & Hautcoeur Valenciennes</title>
 
 	<?php include 'includes/styles.php' ?>
 
+    <link rel="stylesheet" href="styles/css/devis.css">
 </head>
 <body>
 
 <div class="off-canvas-wrapper">
-	<div class="off-canvas-wrapper-inner" data-off-canvas-wrapper>
+    <div class="off-canvas-wrapper-inner" data-off-canvas-wrapper>
 
-		<!-- OFF-CANVAS DISPLAY ON MOBILE -->
+        <!-- OFF-CANVAS DISPLAY ON MOBILE -->
 
 		<?php include 'includes/off-canvas.php' ?>
 
-		<!-- CONTENT OF WEBPAGE -->
-		<div class="off-canvas-content" data-off-canvas-content>
-			<div class="title-bar hide-for-medium">
-				<div class="title-bar-left">
-					<button class="menu-icon-style" type="button" data-open="offCanvas" aria-expanded="false"
-							aria-controls="offCanvas">
-						<i class="fa fa-navicon title-bar__icon"></i>
-					</button>
-				</div>
-			</div>
+        <!-- CONTENT OF WEBPAGE -->
+        <div class="off-canvas-content" data-off-canvas-content>
+            <div class="title-bar hide-for-medium">
+                <div class="title-bar-left">
+                    <button class="menu-icon-style" type="button" data-open="offCanvas" aria-expanded="false" aria-controls="offCanvas">
+                        <i class="fa fa-navicon title-bar__icon"></i>
+                    </button>
+                </div>
+            </div>
 
 			<?php include 'includes/header.php' ?>
 
 			<?php include 'includes/contactInformations.php' ?>
 
-			<!-- CONTENU PRINCIPAL -->
-			<main>
-				<div class="row">
-					<div class="large-12 columns text-center">
-						<h1>Devis en Ligne</h1>
-					</div>
-				</div>
+            <!-- CONTENU PRINCIPAL -->
+            <main>
+                <div class="row">
+                    <div class="large-12 columns text-center">
+                        <h1>Devis en Ligne</h1>
+                    </div>
+                </div>
                 <div class="row">
                     <div class="large-6 columns">
                         <div class="row">
-                            <div class="large-6 columns">
-                                1
+                            <div class="large-6 columns thumbnail devis-image-block">
+                                <a href="#">
+                                    <img src="https://placehold.it/2000x2000&text=Image"/>
+                                    <div class="devis-text-block text-center">
+                                        <p>utem tamque multiplici fertilitate abundat rerum omnium eadem Cyprus ut nullius </p>
+                                    </div>
+                                </a>
                             </div>
-                            <div class="large-6 columns">
-                                2
+                            <div class="large-6 columns thumbnail devis-image-block">
+                                <a href="#">
+                                    <img src="https://placehold.it/2000x2000&text=Image"/>
+                                    <div class="devis-text-block text-center">
+                                        <p>utem tamque multiplici fertilitate abundat rerum omnium eadem Cyprus ut nullius </p>
+                                    </div>
+                                </a>
                             </div>
                         </div>
                         <div class="row">
-                            <div class="large-6 columns">
-                                3
+                            <div class="large-6 columns thumbnail devis-image-block">
+                                <a href="#">
+                                    <img src="https://placehold.it/2000x2000&text=Image"/>
+                                    <div class="devis-text-block text-center">
+                                        <p>utem tamque multiplici fertilitate abundat rerum omnium eadem Cyprus ut nullius </p>
+                                    </div>
+                                </a>
                             </div>
-                            <div class="large-6 columns">
-                                4
+                            <div class="large-6 columns thumbnail devis-image-block">
+                                <a href="#">
+                                    <img src="https://placehold.it/2000x2000&text=Image"/>
+                                    <div class="devis-text-block text-center">
+                                        <p>utem tamque multiplici fertilitate abundat rerum omnium eadem Cyprus ut nullius </p>
+                                    </div>
+                                </a>
                             </div>
                         </div>
                     </div>
                     <div class="large-6 columns">
                         contact
                     </div>
-			</main>
+            </main>
 
 			<?php include 'includes/footer.php' ?>
-		</div>
-	</div>
+        </div>
+    </div>
 </div>
 <script></script>
 
