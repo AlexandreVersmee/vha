7501f409787f93f7b2f9b40030ddfcf42cbed422
VersFred <fsvs59@free.fr>
2017-03-19 17:33:10 +0100

    MISE EN PAGE - PARTICULIERS

--- assurances_particuliers.php
+++ assurances_particuliers.php
@@ -41,29 +41,40 @@
 				</div>
 
 				<div class="row">
-					<div class="large-12 columns">
-						Cyprum itidem insulam procul a continenti discretam et portuosam inter municipia crebra urbes duae faciunt claram Salamis et Paphus,
-						altera Iovis delubris altera Veneris templo insignis. tanta autem tamque multiplici fertilitate abundat rerum omnium eadem Cyprus ut
-						nullius externi indigens adminiculi indigenis viribus a fundamento ipso carinae ad supremos usque carbasos aedificet onerariam navem
-						omnibusque armamentis instructam mari committat.
-
-						Ibi victu recreati et quiete, postquam abierat timor, vicos opulentos adorti equestrium adventu cohortium, quae casu propinquabant, nec
-						resistere planitie porrecta conati digressi sunt retroque concedentes omne iuventutis robur relictum in sedibus acciverunt.
-
-						Sed laeditur hic coetuum magnificus splendor levitate paucorum incondita, ubi nati sunt non reputantium, sed tamquam indulta licentia
-						vitiis ad errores lapsorum ac lasciviam. ut enim Simonides lyricus docet, beate perfecta ratione vieturo ante alia patriam esse convenit
-						gloriosam.
-
-						Hanc regionem praestitutis celebritati diebus invadere parans dux ante edictus per solitudines Aboraeque amnis herbidas ripas, suorum
-						indicio proditus, qui admissi flagitii metu exagitati ad praesidia descivere Romana. absque ullo egressus effectu deinde tabescebat
-						immobilis.
-
-						Inter has ruinarum varietates a Nisibi quam tuebatur accitus Vrsicinus, cui nos obsecuturos iunxerat imperiale praeceptum, dispicere
-						litis exitialis certamina cogebatur abnuens et reclamans, adulatorum oblatrantibus turmis, bellicosus sane milesque semper et militum
-						ductor sed forensibus iurgiis longe discretus, qui metu sui discriminis anxius cum accusatores quaesitoresque subditivos sibi
-						consociatos ex isdem foveis cerneret emergentes, quae clam palamve agitabantur, occultis Constantium litteris edocebat inplorans
-						subsidia, quorum metu tumor notissimus Caesaris exhalaret.
+					<div class="small-12 medium-6 large-4 columns">
+					<img src="https://placehold.it/2000x2000&text=Image1"/>
 					</div>
+					<div class="small-12 medium-6 large-4 columns">
+					<img src="https://placehold.it/2000x2000&text=Image2"/>
+					</div>
+					<div class="small-12 medium-6 large-4 columns">
+					<img src="https://placehold.it/2000x2000&text=Image3"/>
+					</div>
+					<div class="small-12 medium-6 large-4 columns">
+					<img src="https://placehold.it/2000x2000&text=Image4"/>
+					</div>
+					<div class="small-12 medium-6 large-4 columns">
+					<img src="https://placehold.it/2000x2000&text=Image5"/>
+					</div>
+					<div class="small-12 medium-6 large-4 columns">
+					<img src="https://placehold.it/2000x2000&text=Image6"/>
+					</div>
+					<div class="small-12 medium-6 large-4 columns">
+					<img src="https://placehold.it/2000x2000&text=Image7"/>
+					</div>
+					<div class="small-12 medium-6 large-4 columns">
+					<img src="https://placehold.it/2000x2000&text=Image8"/>
+					</div>
+					<div class="small-12 medium-6 large-4 columns">
+					<img src="https://placehold.it/2000x2000&text=Image1"/>
+					</div>
+					<div class="small-12 medium-6 large-4 columns">
+					<img src="https://placehold.it/2000x2000&text=Image2"/>
+					</div>
+					<div class="small-12 medium-6 large-4 end columns">
+					<img src="https://placehold.it/2000x2000&text=Image3"/>
+					</div>
+
 				</div>
 			</main>
 
