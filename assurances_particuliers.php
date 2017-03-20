4b048cf8633edd950dfdda5d39a1e0dc9ed24efa
VersFred <fsvs59@free.fr>
2017-03-20 23:08:10 +0100

    Màj mineures particuliers ajout photos

--- assurances_particuliers.php
+++ assurances_particuliers.php
@@ -1,39 +1,39 @@
 <!DOCTYPE html>
 <html class="no-js" lang="en" dir="ltr">
 <head>
-	<?php include 'includes/metas.php' ?>
+    <?php include 'includes/metas.php' ?>
 
-	<title>Assurance Versmée & Hautcoeur Valenciennes</title>
+    <title>Assurance Versmée & Hautcoeur Valenciennes</title>
 
-	<?php include 'includes/styles.php'?>
+    <?php include 'includes/styles.php' ?>
 
 </head>
 <body>
 
 <div class="off-canvas-wrapper">
-	<div class="off-canvas-wrapper-inner" data-off-canvas-wrapper>
+    <div class="off-canvas-wrapper-inner" data-off-canvas-wrapper>
 
-		<!-- OFF-CANVAS DISPLAY ON MOBILE -->
+        <!-- OFF-CANVAS DISPLAY ON MOBILE -->
 
-		<?php include 'includes/off-canvas.php' ?>
+        <?php include 'includes/off-canvas.php' ?>
 
-		<!-- CONTENT OF WEBPAGE -->
-		<div class="off-canvas-content" data-off-canvas-content>
-			<div class="title-bar hide-for-medium">
-				<div class="title-bar-left">
-					<button class="menu-icon-style" type="button" data-open="offCanvas" aria-expanded="false"
-							aria-controls="offCanvas">
-						<i class="fa fa-navicon title-bar__icon"></i>
-					</button>
-				</div>
-			</div>
+        <!-- CONTENT OF WEBPAGE -->
+        <div class="off-canvas-content" data-off-canvas-content>
+            <div class="title-bar hide-for-medium">
+                <div class="title-bar-left">
+                    <button class="menu-icon-style" type="button" data-open="offCanvas" aria-expanded="false"
+                            aria-controls="offCanvas">
+                        <i class="fa fa-navicon title-bar__icon"></i>
+                    </button>
+                </div>
+            </div>
 
-			<?php include 'includes/header.php' ?>
+            <?php include 'includes/header.php' ?>
 
-			<?php include 'includes/contactInformations.php' ?>
+            <?php include 'includes/contactInformations.php' ?>
 
-			<!-- CONTENU PRINCIPAL -->
-			<main>
+            <!-- CONTENU PRINCIPAL -->
+            <main>
                 <div class="row">
                     <div class="small-12 columns page-title">
                         <h1><span>Assurances Particuliers</span></h1>
@@ -41,49 +41,49 @@
                 </div>
 
 
-				<div class="row">
+                <div class="row">
 
-					<div class="small-12 medium-6 large-4 columns">
+                    <div class="small-12 medium-6 large-4 columns">
 
-					<img src="https://placehold.it/2000x2000&text=Image1"/>
-					</div>
-					<div class="small-12 medium-6 large-4 columns">
-					<img src="https://placehold.it/2000x2000&text=Image2"/>
-					</div>
-					<div class="small-12 medium-6 large-4 columns">
-					<img src="https://placehold.it/2000x2000&text=Image3"/>
-					</div>
-					<div class="small-12 medium-6 large-4 columns">
-					<img src="https://placehold.it/2000x2000&text=Image4"/>
-					</div>
-					<div class="small-12 medium-6 large-4 columns">
-					<img src="https://placehold.it/2000x2000&text=Image5"/>
-					</div>
-					<div class="small-12 medium-6 large-4 columns">
-					<img src="https://placehold.it/2000x2000&text=Image6"/>
-					</div>
-					<div class="small-12 medium-6 large-4 columns">
-					<img src="https://placehold.it/2000x2000&text=Image7"/>
-					</div>
-					<div class="small-12 medium-6 large-4 columns">
-					<img src="https://placehold.it/2000x2000&text=Image8"/>
-					</div>
-					<div class="small-12 medium-6 large-4 columns">
-					<img src="https://placehold.it/2000x2000&text=Image1"/>
-					</div>
-					<div class="small-12 medium-6 large-4 columns">
-					<img src="https://placehold.it/2000x2000&text=Image2"/>
-					</div>
-					<div class="small-12 medium-6 large-4 end columns">
-					<img src="https://placehold.it/2000x2000&text=Image3"/>
-					</div>
+                        <img src="images/auto-petit-rouleur-280x165.jpg"/>
+                    </div>
+                    <div class="small-12 medium-6 large-4 columns">
+                        <img src="images/auto-malusse-2-280x165.jpg"/>
+                    </div>
+                    <div class="small-12 medium-6 large-4 columns">
+                        <img src="images/auto-propre-280x165.jpg"/>
+                    </div>
+                    <div class="small-12 medium-6 large-4 columns">
+                        <img src="images/auto-vehicule-pro-280x165.jpg"/>
+                    </div>
+                    <div class="small-12 medium-6 large-4 columns">
+                        <img src="images/Auto_conduite-connectee_280x165.jpg"/>
+                    </div>
+                    <div class="small-12 medium-6 large-4 columns">
+                        <img src="https://placehold.it/2000x2000&text=Image6"/>
+                    </div>
+                    <div class="small-12 medium-6 large-4 columns">
+                        <img src="images/Auto_conduite-connectee_280x165.jpg"/>
+                    </div>
+                    <div class="small-12 medium-6 large-4 columns">
+                        <img src="https://placehold.it/2000x2000&text=Image8"/>
+                    </div>
+                    <div class="small-12 medium-6 large-4 columns">
+                        <img src="https://placehold.it/2000x2000&text=Image1"/>
+                    </div>
+                    <div class="small-12 medium-6 large-4 columns">
+                        <img src="https://placehold.it/2000x2000&text=Image2"/>
+                    </div>
+                    <div class="small-12 medium-6 large-4 end columns">
+                        <img src="https://placehold.it/2000x2000&text=Image3"/>
+                    </div>
 
-				</div>
-			</main>
+                </div>
+            </main>
 
-			<?php include 'includes/footer.php' ?>
-		</div>
-	</div>
+            <?php include 'includes/footer.php' ?>
+        </div>
+    </div>
 </div>
 <script></script>
 
