ba6c282c1b8a8edce975b24dd4ffe6f9eec9acd2
Alex <versmee.alexandre@gmail.com>
2017-03-22 20:20:30 +0100

    Test page assurance particuliers

--- assurances_particuliers.php
+++ assurances_particuliers.php
@@ -1,12 +1,13 @@
 <!DOCTYPE html>
 <html class="no-js" lang="en" dir="ltr">
 <head>
-    <?php include 'includes/metas.php' ?>
+	<?php include 'includes/metas.php' ?>
 
     <title>Assurance Versmée & Hautcoeur Valenciennes</title>
 
-    <?php include 'includes/styles.php' ?>
+	<?php include 'includes/styles.php' ?>
 
+    <link rel="stylesheet" href="styles/css/products.css">
 </head>
 <body>
 
@@ -15,22 +16,21 @@
 
         <!-- OFF-CANVAS DISPLAY ON MOBILE -->
 
-        <?php include 'includes/off-canvas.php' ?>
+		<?php include 'includes/off-canvas.php' ?>
 
         <!-- CONTENT OF WEBPAGE -->
         <div class="off-canvas-content" data-off-canvas-content>
             <div class="title-bar hide-for-medium">
                 <div class="title-bar-left">
-                    <button class="menu-icon-style" type="button" data-open="offCanvas" aria-expanded="false"
-                            aria-controls="offCanvas">
+                    <button class="menu-icon-style" type="button" data-open="offCanvas" aria-expanded="false" aria-controls="offCanvas">
                         <i class="fa fa-navicon title-bar__icon"></i>
                     </button>
                 </div>
             </div>
 
-            <?php include 'includes/header.php' ?>
+			<?php include 'includes/header.php' ?>
 
-            <?php include 'includes/contactInformations.php' ?>
+			<?php include 'includes/contactInformations.php' ?>
 
             <!-- CONTENU PRINCIPAL -->
             <main>
@@ -40,111 +40,127 @@
                     </div>
                 </div>
 
-<div class="row">
-    <div class="small-4 columns">
-        <!-- Requires Foundation 4 | visit http://foundation.zurb.com to download -->
-        <div class="card">
-            <div class="container">
-                <div class="front">
-                    <img src="http://placehold.it/400x200" />
-                </div>
+                <div class="row cards-row">
+                    <h3 class="cards-title">
+                        <span class="fa-stack fa-lg particulier">
+                            <i class="fa fa-square-o fa-stack-2x"></i>
+                            <i class="fa fa-user fa-stack-1x"></i>
+                        </span>
+                        A chaque âge ses solutions ...
+                    </h3>
 
-                <div class="back">
-                    <h5>This is a card.</h5>
-                    <p>This card shows some content on the front and some on the back, and can be flipped by hovering on a desktop, or by swiping across on a touch device.</p>
-                    <p>The back of the card is intentionally long, so that we can show both an example where the card is constrained to a square and where it is as long as the content on either side requires.</p>
-                    <p>This card shows some content on the front and some on the back, and can be flipped by hovering on a desktop, or by swiping across on a touch device.</p>
-                    <p>The back of the card is intentionally long, so that we can show both an example where the card is constrained to a square and where it is as long as the content on either side requires.</p>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="small-4 columns">
-        <!-- Requires Foundation 4 | visit http://foundation.zurb.com to download -->
-        <div class="card">
-            <div class="container">
-                <div class="front">
-                    <img src="http://placehold.it/400x200" />
-                </div>
+                    <div class="small-4 columns">
+                        <div class="card">
+                            <div class="container">
+                                <div class="front">
+                                    <img class="thumbnail" src="http://placehold.it/400x250"/>
+                                </div>
 
-                <div class="back">
-                    <h5>This is a card.</h5>
-                    <p>This card shows some content on the front and some on the back, and can be flipped by hovering on a desktop, or by swiping across on a touch device.</p>
-                    <p>The back of the card is intentionally long, so that we can show both an example where the card is constrained to a square and where it is as long as the content on either side requires.</p>
-                    <p>This card shows some content on the front and some on the back, and can be flipped by hovering on a desktop, or by swiping across on a touch device.</p>
-                    <p>The back of the card is intentionally long, so that we can show both an example where the card is constrained to a square and where it is as long as the content on either side requires.</p>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="small-4 columns">
-        <!-- Requires Foundation 4 | visit http://foundation.zurb.com to download -->
-        <div class="card">
-            <div class="container">
-                <div class="front">
-                    <img src="http://placehold.it/400x200" />
-                </div>
+                                <div class="back">
+                                    <h5>This is a card.</h5>
+                                    <p>This card shows some content on the front and some on the back, and can be flipped by hovering on a desktop, or by
+                                       swiping across on a touch device. swiping across on a touch device. swiping across on a touch device.
+                                       swiping across on a touch device. </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="small-4 columns">
+                        <div class="card">
+                            <div class="container">
+                                <div class="front">
+                                    <img class="thumbnail" src="http://placehold.it/400x250"/>
+                                </div>
 
-                <div class="back">
-                    <h5>This is a card.</h5>
-                    <p>This card shows some content on the front and some on the back, and can be flipped by hovering on a desktop, or by swiping across on a touch device.</p>
-                    <p>The back of the card is intentionally long, so that we can show both an example where the card is constrained to a square and where it is as long as the content on either side requires.</p>
-                    <p>This card shows some content on the front and some on the back, and can be flipped by hovering on a desktop, or by swiping across on a touch device.</p>
-                    <p>The back of the card is intentionally long, so that we can show both an example where the card is constrained to a square and where it is as long as the content on either side requires.</p>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+                                <div class="back">
+                                    <h5>This is a card.</h5>
+                                    <p>This card shows some content on the front and some on the back, and can be flipped by hovering on a desktop, or by
+                                       swiping across on a touch device. swiping across on a touch device. swiping across on a touch device.
+                                       swiping across on a touch device. </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="small-4 columns">
+                        <div class="card">
+                            <div class="container">
+                                <div class="front">
+                                    <img class="thumbnail" src="http://placehold.it/400x250"/>
+                                </div>
 
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
+                                <div class="back">
+                                    <h5>This is a card.</h5>
+                                    <p>This card shows some content on the front and some on the back, and can be flipped by hovering on a desktop, or by
+                                       swiping across on a touch device. swiping across on a touch device. swiping across on a touch device.
+                                       swiping across on a touch device. </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
                 <div class="row">
-                    <div class="small-12 medium-6 large-4 columns">
+                    <h3 class="products-title">
+                        <span class="fa-stack fa-lg particulier">
+                            <i class="fa fa-square-o fa-stack-2x"></i>
+                            <i class="fa fa-user fa-stack-1x"></i>
+                        </span>
+                        Les solutions
+                    </h3>
 
-                        <img src="images/auto-petit-rouleur-280x165.jpg"/>
+                    <div class="small-12 medium-6 large-4 columns product">
+                        <img class="thumbnail " src="https://placehold.it/2000x1000&text=Image1"/>
+                        <p class="text-center">Assurance Auto</p>
                     </div>
-                    <div class="small-12 medium-6 large-4 columns">
-                        <img src="images/auto-malusse-2-280x165.jpg"/>
+                    <div class="small-12 medium-6 large-4 columns product">
+                        <img class="thumbnail" src="https://placehold.it/2000x1000&text=Image2"/>
+                        <p class="text-center">Assurance Auto</p>
                     </div>
-                    <div class="small-12 medium-6 large-4 columns">
-                        <img src="images/auto-propre-280x165.jpg"/>
+                    <div class="small-12 medium-6 large-4 columns product">
+                        <img class="thumbnail" src="https://placehold.it/2000x1000&text=Image3"/>
+                        <p class="text-center">Assurance Auto</p>
                     </div>
-                    <div class="small-12 medium-6 large-4 columns">
-                        <img src="images/auto-vehicule-pro-280x165.jpg"/>
+                    <div class="small-12 medium-6 large-4 columns product">
+                        <img class="thumbnail" src="https://placehold.it/2000x1000&text=Image4"/>
+                        <p class="text-center">Assurance Auto</p>
                     </div>
-                    <div class="small-12 medium-6 large-4 columns">
-                        <img src="images/Auto_conduite-connectee_280x165.jpg"/>
+                    <div class="small-12 medium-6 large-4 columns product">
+                        <img class="thumbnail" src="https://placehold.it/2000x1000&text=Image5"/>
+                        <p class="text-center">Assurance Auto</p>
                     </div>
-                    <div class="small-12 medium-6 large-4 columns">
-                        <img src="https://placehold.it/2000x2000&text=Image6"/>
+                    <div class="small-12 medium-6 large-4 columns product">
+                        <img class="thumbnail" src="https://placehold.it/2000x1000&text=Image6"/>
+                        <p class="text-center">Assurance Auto</p>
                     </div>
-                    <div class="small-12 medium-6 large-4 columns">
-                        <img src="images/Auto_conduite-connectee_280x165.jpg"/>
+                    <div class="small-12 medium-6 large-4 columns product">
+                        <img class="thumbnail" src="https://placehold.it/2000x1000&text=Image7"/>
+                        <p class="text-center">Assurance Auto</p>
                     </div>
-                    <div class="small-12 medium-6 large-4 columns">
-                        <img src="https://placehold.it/2000x2000&text=Image8"/>
+                    <div class="small-12 medium-6 large-4 columns product">
+                        <img class="thumbnail" src="https://placehold.it/2000x1000&text=Image8"/>
+                        <p class="text-center">Assurance Auto</p>
                     </div>
-                    <div class="small-12 medium-6 large-4 columns">
-                        <img src="https://placehold.it/2000x2000&text=Image1"/>
+                    <div class="small-12 medium-6 large-4 columns product">
+                        <img class="thumbnail" src="https://placehold.it/2000x1000&text=Image9"/>
+                        <p class="text-center">Assurance Auto</p>
                     </div>
-                    <div class="small-12 medium-6 large-4 columns">
-                        <img src="https://placehold.it/2000x2000&text=Image2"/>
+                    <div class="small-12 medium-6 large-4 columns product">
+                        <img class="thumbnail" src="https://placehold.it/2000x1000&text=Image10"/>
+                        <p class="text-center">Assurance Auto</p>
                     </div>
                     <div class="small-12 medium-6 large-4 end columns">
-                        <img src="https://placehold.it/2000x2000&text=Image3"/>
+                        <img class="thumbnail" src="https://placehold.it/2000x1000&text=Image11"/>
+                        <p class="text-center">Assurance Auto</p>
                     </div>
-
+                    <div class="small-12 medium-6 large-4 end columns">
+                        <img class="thumbnail" src="https://placehold.it/2000x1000&text=Image12"/>
+                        <p class="text-center">Assurance Auto</p>
+                    </div>
                 </div>
+
             </main>
 
-            <?php include 'includes/footer.php' ?>
+			<?php include 'includes/footer.php' ?>
         </div>
     </div>
 </div>
