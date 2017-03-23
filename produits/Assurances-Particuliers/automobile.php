88265ea9a83db49e94da8390523d3bbbf221a705
Alex <versmee.alexandre@gmail.com>
2017-03-23 19:03:45 +0100

    Assurance_particulier : pages des différents produits

---
+++ produits/Assurances-Particuliers/automobile.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html class="no-js" lang="en" dir="ltr">
+<head>
+	<?php include '../../includes/metas.php' ?>
+
+	<title>Assurance Versmée & Hautcoeur Valenciennes</title>
+
+	<?php include '../../includes/styles.php' ?>
+</head>
+<body>
+
+<div class="off-canvas-wrapper">
+	<div class="off-canvas-wrapper-inner" data-off-canvas-wrapper>
+
+		<!-- OFF-CANVAS DISPLAY ON MOBILE -->
+
+		<?php include '../../includes/off-canvas.php' ?>
+
+		<!-- CONTENT OF WEBPAGE -->
+		<div class="off-canvas-content" data-off-canvas-content>
+			<div class="title-bar hide-for-medium">
+				<div class="title-bar-left">
+					<button class="menu-icon-style" type="button" data-open="offCanvas" aria-expanded="false" aria-controls="offCanvas">
+						<i class="fa fa-navicon title-bar__icon"></i>
+					</button>
+				</div>
+			</div>
+
+			<?php include '../../includes/header.php' ?>
+
+			<?php include '../../includes/contactInformations.php' ?>
+
+			<!-- CONTENU PRINCIPAL -->
+			<main>
+				<div class="row">
+					<div class="small-12 columns page-title">
+						<h1><span>Assurances Automobile</span></h1>
+					</div>
+				</div>
+
+			</main>
+
+			<?php include '../../includes/footer.php' ?>
+		</div>
+	</div>
+</div>
+
+<!-- SCRIPTS -->
+<?php include '../../includes/scripts.php' ?>
+
+</body>
+</html>
